7c9dde37ea94de6a3ba8829355cfdc547a536f05
Kuniaki IGARASHI <igaiga@gmail.com>
2015-08-03 09:18:47 +0900

    Add emoji

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,11 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
+    g params[:book]
+    g "💛💙💜💚💔💗💓💕💖💞💘"
+    logger.debug "************💛💙💜💚💔💗💓💕💖💞💘"
+    logger.debug params[:book]
+    logger.debug "************💛💙💜💚💔💗💓💕💖💞💘"
     @book = Book.new(book_params)
 
     respond_to do |format|
